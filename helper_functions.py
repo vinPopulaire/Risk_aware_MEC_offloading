8938b1cd2b7dfd1be56e2fd859c8e8e404f279e9
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2019-10-14 10:00:53 +0300

    add function to help tune parameters

--- helper_functions.py
+++ helper_functions.py
@@ -4,6 +4,8 @@ Helper functions that are used in the simulation
 
 import numpy as np
 
+from game_functions import *
+
 def initialize(N, **params):
     '''
     Initialize the probabilities for the simulation
@@ -24,7 +26,7 @@ def initialize(N, **params):
         to the MEC server.
     '''
 
-    b = 0.5 * np.ones(N)
+    b = 3000000 * np.ones(N)
 
     # set b_old different to b so that we don't falsely converge
     b_old = 1 * np.ones(N)
@@ -57,3 +59,84 @@ def set_costs(b, bn, dn, c, **params):
     costs = c * dn * b/bn
 
     return costs
+
+def check_game(bn, dn, an, kn, c, tn, en, **params):
+
+    working = 0
+    for an1 in np.linspace(0.2, 1, 9):
+        an = an1
+
+        for kn1 in np.linspace(0.1, 2, 10):
+            kn = kn1
+            working += 1
+            print(working)
+
+            # for dn1 in np.linspace(1, 10, 10):
+            #     dn = dn1 * 1e9 * np.ones(params["N"])
+
+            #     for fn1 in np.linspace(1, 10, 10):
+            #         fn = fn1 * 1e9 * np.ones(params["N"])
+            #         tn = dn/fn
+
+            #         for gn1 in np.linspace(1, 10, 10):
+            #             gn = gn1 * 1e-9 * np.ones(params["N"])
+            #             en = gn * dn
+
+            for bn1 in np.linspace(1, 10, 10):
+                bn = bn1 * 1e6 * np.ones(params["N"])
+
+                for c1 in np.linspace(2e-1, 9e-1, 8):
+                    c = c1 * bn/dn * (1 - (1/(tn*en)))
+
+                    if c.all() > 0:
+
+                        N = 10000
+
+                        # fig, ax = plt.subplots(11,1)
+
+                        counter = 0
+
+                        for i, value in enumerate(np.linspace(0,bn[0],11)):
+                            b_others = value * np.ones(5)
+
+                            x = np.linspace(0, bn[0], N)
+                            res = np.empty_like(x)
+
+                            for j in range(len(x)):
+                                res[j] = -utility_function(x[j], 0, b_others, dn, bn, an, kn, c, tn, en)
+
+                            if (res[0] < res[1]) and (res[-2] > res[-1]):
+                                counter += 1
+
+                            # plt.subplot(11,1,i+1)
+                            # plt.suptitle("an: " + str(an) +
+                            #         " bn: " + str(bn[0]) +
+                            #         " dn: " + str(dn[0]) +
+                            #         " kn: " + str(kn) +
+                            #         " tn: " + str(tn[0]) +
+                            #         " en: " + str(en[0]) +
+                            #         " c: " + str(c), fontsize=6)
+                            # plt.plot(x, res)
+
+                        if counter > 8:
+                            my_string = ("Counter: " + str(counter) + "\n" +
+                                "an: " + str(an) +
+                                " bn: " + str(bn[0]) +
+                                " dn: " + str(dn[0]) +
+                                " kn: " + str(kn) +
+                                " tn: " + str(tn[0]) +
+                                " en: " + str(en[0]) +
+                                " c: " + str(c1) + "\n")
+
+                            with open('result.txt' , 'a') as writer:
+                                writer.write(my_string)
+
+                            # print(my_string)
+
+                            # kati = np.argmax(res)
+                            # print(x[kati])
+                            # print()
+
+    # plt.show(block=False)
+
+    return "Done"
