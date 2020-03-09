e8f7209cea2bc1209df5f850c1c654f709addd53
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2020-03-09 12:46:43 +0200

    add simulations for different ans and kns

--- simulation.py
+++ simulation.py
@@ -28,6 +28,12 @@ Ns = [25]
 # cpars = np.linspace(0.1,0.9,33)
 cpars = [0.5]
 
+ans = [0.2]
+# ans = np.linspace(0.1,1,10)
+
+kns = [1.2]
+# kns = np.linspace(0.2,2,10)
+
 for N in Ns:
     # Set random parameter in order to generate the same parameters
     print("Generating new parameters")
@@ -36,56 +42,68 @@ for N in Ns:
 
     print("Number of users: " + str(params["N"]))
 
-    for cpar in cpars:
+    for an in ans:
 
-        params["cpar"] = cpar
-        params["c"] = cpar * params["bn"]/params["dn"] * (1 - (1/(params["tn"]*params["en"])))
+        params["an"] = an
 
-        print("Cost parameter: "+ str(params["cpar"]))
+        print("Sensitivity an: " + str(params["an"]))
 
-        # if bpars is -1 we don't want constant offloading
-        bpars = [-1,0,0.5,1] if params["CONSTANT_OFFLOADING"] else [-1]
+        for kn in kns:
 
-        while bpars:
-            bpar = bpars.pop()
-            params["bpar"] = bpar
+            params["kn"] = kn
 
-            # if bpar is -1 then we don't want constant offloading
-            if bpar == -1:
-                params["CONSTANT_OFFLOADING"] = False
-                params["bpar"] = 0
+            print("Sensitivity kn: " + str(params["kn"]))
 
-            for repetition in range(1):
-                print("Repetition no: " + str(repetition+1))
+            for cpar in cpars:
 
-                results = {}
+                params["cpar"] = cpar
+                params["c"] = cpar * params["bn"]/params["dn"] * (1 - (1/(params["tn"]*params["en"])))
 
-                start = time.time()
+                print("Cost parameter: "+ str(params["cpar"]))
 
-                # Run main simulation
-                results = main(params)
-                # check_all_parameters(**params)
-                # check_best_parameters(**params)
+                # if bpars is -1 we don't want constant offloading
+                bpars = [-1,0,0.5,1] if params["CONSTANT_OFFLOADING"] else [-1]
 
-                end = time.time()
-                running_time = end - start
-                print("Time of simulation:")
-                print(running_time)
+                while bpars:
+                    bpar = bpars.pop()
+                    params["bpar"] = bpar
+
+                    # if bpar is -1 then we don't want constant offloading
+                    if bpar == -1:
+                        params["CONSTANT_OFFLOADING"] = False
+                        params["bpar"] = 0
+
+                    for repetition in range(1):
+                        print("Repetition no: " + str(repetition+1))
+
+                        results = {}
+
+                        start = time.time()
+
+                        # Run main simulation
+                        results = main(params)
+                        # check_all_parameters(**params)
+                        # check_best_parameters(**params)
+
+                        end = time.time()
+                        running_time = end - start
+                        print("Time of simulation:")
+                        print(running_time)
 
 
-                results["N"] = N
-                results["time"] = running_time
-                results["repetition"] = repetition
+                        results["N"] = N
+                        results["time"] = running_time
+                        results["repetition"] = repetition
 
-                if params["SAVE_RESULTS"] == True:
-                    if params["CONSTANT_OFFLOADING"]:
-                        constant_str = "_b_constant_" + str(round(bpar,3))
-                    else:
-                        constant_str = ""
-                    outfile = 'saved_runs/results/individual/' + case["users"] + constant_str + "_N_" + str(N) + "_c_" + str(round(cpar,3)) + "_" + str(repetition)
+                        if params["SAVE_RESULTS"] == True:
+                            if params["CONSTANT_OFFLOADING"]:
+                                constant_str = "_b_constant_" + str(round(bpar,3))
+                            else:
+                                constant_str = ""
+                            outfile = 'saved_runs/results/individual/' + case["users"] + constant_str + "_N_" + str(N) + "_an_" + str(round(an,3)) + "_kn_" + str(round(kn,3)) + "_c_" + str(round(cpar,3)) + "_" + str(repetition)
 
-                    with open(outfile, 'wb') as fp:
-                        dill.dump(results, fp)
+                            with open(outfile, 'wb') as fp:
+                                dill.dump(results, fp)
 
 if params["GENERATE_FIGURES"] and not params["SAVE_FIGS"]:
     plt.show()
